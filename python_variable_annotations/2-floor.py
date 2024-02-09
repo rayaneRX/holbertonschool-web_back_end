fbccb5bbaf86cbc4be9e906d019e6cc914140697
rayaneRX <rayane@rayane>
2024-02-09 07:02:25 +0100

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import math
+
+def floor(n: float) -> int:
+    """
+     function floor which takes a float n as argument
+     and returns the floor of the float.
+    """
+    return int(n)
