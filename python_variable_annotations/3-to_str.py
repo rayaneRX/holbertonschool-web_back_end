87299402a494fb3125e278748f54aafe0906d733
rayaneRX <rayane@rayane>
2024-02-09 07:35:23 +0100

    push all task

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -8,5 +8,4 @@ def to_str(n: float) -> str:
     ''' to_str that takes a float n as argument and
     returns the string representation of the float. '''
 
-
     return str(n)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,6 +7,7 @@
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
         function sum_list
