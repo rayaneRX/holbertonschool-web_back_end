fe4ec3982c1bbda70872118fa0f9173d67d9dfc1
rayaneRX <rayane@rayane>
2024-02-22 08:40:22 +0100

    task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,10 +7,11 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """"
-    function called filter_datum that returns the log message obfuscate
-	"""
-    pattern = '|'.join(fields)
-    return re.sub(r'({})=([^{}]+)'.format(pattern, re.escape(separator)), r'\1={}'.format(redaction), message)
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
 
+	"""function called filter_datum that returns the log message obfuscate"""
+
+	pattern = '|'.join(fields)
+	return re.sub(r'({})=([^{}]+)'.format(pattern, re.escape(separator)),
+			   r'\1={}'.format(redaction), message)
