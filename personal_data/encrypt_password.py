bf8c3be10463d3d72e6c04e2617647a222b5ce7f
rayaneRX <rayane@rayane>
2024-02-23 14:15:52 +0100

    task 0

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ Module for implementing a hash_password function """
-
 import bcrypt
 
 
-def hash_password(password) -> bytes:
+def hash_password(password: str) -> bytes:
     """ Module for implementing a hash_password function """
     password_bytes = password.encode()
     salt = bcrypt.gensalt()
