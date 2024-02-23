1eb7dc57bd233ea14c59a051aa8c214a3a3f1583
rayaneRX <rayane@rayane>
2024-02-23 14:11:01 +0100

    task 0

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Encrypt a string
+"""
+
+
+import bcrypt
+
+def hash_password(password):
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
