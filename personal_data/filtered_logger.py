f4cf96deba53efb7e7cd169326ba28cc1aeec330
rayaneRX <rayane@rayane>
2024-02-23 11:47:09 +0100

    pycode

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ import logging
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
-    Replace specified fields in the log message with the redaction string.
+    Update the class to accept a list of strings fields constructor argument.
     """
     return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
                   fr'\1={redaction}',
@@ -20,18 +20,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-    Repla
+    """ Redacting Formatter class
     """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        Args:
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Format the log record.
+        """
         record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
                                   self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
