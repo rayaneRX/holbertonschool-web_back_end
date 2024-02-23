56423a1da0aa7da5bd156be9e7964523c1178fa1
rayaneRX <rayane@rayane>
2024-02-23 13:43:46 +0100

    task 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,49 +5,36 @@
 
 
 import logging
-from logging import StreamHandler
-from typing import List
 import csv
-from datetime import datetime
 
-
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """
-    Replace specified fields in the log message with the redaction string.
-    """
-    return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
-                  fr'\1={redaction}',
-                  message)
+PII_FIELDS = ("name", "email", "phone", "address", "creditcard")
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+    def __init__(self, fields):
+        super().__init__()
         self.fields = fields
 
-    def format(self, record: logging.LogRecord) -> str:
-        """ Format the log record. """
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
-                                  self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+    def format(self, record):
+        message = super().format(record)
+        for field in self.fields:
+            message = message.replace(field, "****")
+        return message
 
 
-def get_logger() -> logging.Logger:
-    """Return a logging.Logger object."""
+def get_logger():
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
-    handler = StreamHandler()
     formatter = RedactingFormatter(PII_FIELDS)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
     logger.propagate = False
     return logger
 
 
-PII_FIELDS = ("name", "email", "phone", "address", "credit_card")
+if __name__ == "__main__":
+    logger = get_logger()
+    logger.info("This is a test message with sensitive information: "
+                "name=bob; email=bob@example.com; phone=123456789; "
+                "address=123 Street; creditcard=1234567890123456")
