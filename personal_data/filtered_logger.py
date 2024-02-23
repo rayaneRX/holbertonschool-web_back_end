a62c0d2e6797e8a9b94f366a078e56b392725309
rayaneRX <rayane@rayane>
2024-02-23 11:06:59 +0100

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,7 @@
 
 
 import logging
+from typing import List
 from filtered_logger import filter_datum
 
 
@@ -15,7 +16,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
