22f38a2ba43404ed22a3002defab810515527ebf
rayaneRX <rayane@rayane>
2024-02-23 14:02:38 +0100

    task 3

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,27 +1,77 @@
 #!/usr/bin/env python3
 """
-Main file
+Personal data
 """
-
+import re
+import logging
+from typing import List
+from typing import Tuple
 import os
 import mysql.connector
+from mysql.connector.connection import MySQLConnection
+
+PII_FIELDS: List[str] = ["name", "email", "phone_number", "address",
+                         "social_security_number"]
 
 
-def get_db():
-    """Returns a connector to the MySQL database."""
-    # Retrieve database credentials from environment variables
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Returns the log message obfuscated
+    """
+    return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
+                  fr'\1={redaction}',
+                  message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes a RedactingFormatter instance with a list
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the log record, obfuscating specified fields
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+def get_logger() -> logging.Logger:
+    """Returns a logging.Logger object named "user_data" with
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> MySQLConnection:
+    """Returns a connector to the database.
+    """
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-
-    # Connect to the database
-    return mysql.connector.connect(
-        host=host,
+    database = os.getenv('PERSONAL_DATA_DB_NAME', 'my_db')
+    db = mysql.connector.connect(
         user=username,
         password=password,
-        database=db_name
+        host=host,
+        database=database
     )
+    return db
 
 
 if __name__ == "__main__":
