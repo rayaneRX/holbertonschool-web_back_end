b8d98c0f06c4ed87ebf4d640319fe2a97cfcce37
rayaneRX <rayane@rayane>
2024-02-23 14:17:40 +0100

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -9,3 +9,16 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Module for implementing a is_valid function """
+    password_bytes = password.encode()
+    return bcrypt.checkpw(password_bytes, hashed_password)
+
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    encrypted_password = hash_password(password)
+    print(encrypted_password)
+    print(is_valid(encrypted_password, password))
