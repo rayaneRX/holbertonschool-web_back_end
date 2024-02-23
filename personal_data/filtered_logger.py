7450faecbe2587887d1ca6b1268443e53d6d0dc1
rayaneRX <rayane@rayane>
2024-02-23 11:33:32 +0100

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,27 +3,26 @@
  function called filter_datum that returns the log message obfuscate
 """
 
-import logging
+import re
 from typing import List
-
+import logging
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Update the class to accept a list of strings fields constructor argument.
+    """
+    return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
+                  fr'\1={redaction}',
+                  message)
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
-
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
-
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
-    pdate the class to accept a list of strings fields constructor argument.
-    """
-    pattern = fr'({"|".join(fields)})=[^{separator}]+'
-    return re.sub(pattern, fr'\\1={redaction}', message)
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
