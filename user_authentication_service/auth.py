dce4cf281933112a9612680356be04f61fe7e31a
rayaneRX <rayane@rayane>
2024-03-01 16:47:51 +0100

    task 6

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,7 +3,7 @@
 
 from user import User
 import bcrypt
-
+import uuid
 
 class Auth:
     """Auth class to interact with the authentication database."""
@@ -40,3 +40,9 @@ class Auth:
                 return True
 
         return False
+
+    def _generate_uuid() -> str:
+        """
+        Generates a new UUID and returns its string representation.
+        """
+        return str(uuid.uuid4())
