a4155a2e384ba29b2b3208cdbdc00a3501ea7b54
rayaneRX <rayane@rayane>
2024-03-01 11:38:58 +0100

    fix

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python3
-""" DB module """
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
-from user import Base, User
-
+from sqlalchemy.exc import IntegrityError
+from user import User
 
 class DB:
-    """ DB class """
+    """DB class
+    """
 
     def __init__(self) -> None:
-        """ Initialize a new DB instance """
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -20,15 +15,28 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """ Memoized session object """
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a user to the database """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
+        """Add a new user to the database
+        
+        Args:
+            email: Email of the user.
+            hashed_password: Hashed password of the user.
+            
+        Returns:
+            User object that was added.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        try:
+            self._session.commit()
+        except IntegrityError:
+            self._session.rollback()
+            raise ValueError("User with this email already exists")
+        return user
