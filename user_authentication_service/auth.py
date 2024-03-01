e44378ab8b3227f41f61f1a7c95cf91a95d060c5
rayaneRX <rayane@rayane>
2024-03-01 12:53:03 +0100

    Hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Hash password """
+
+import bcrypt
+
+
+def hash_password(password):
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
