e76b468f3971cba5c70440417c90569a294e65a0
rayaneRX <rayane@rayane>
2024-03-01 17:06:03 +0100

    readme

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 """
-This module contains the User class which is a model
-for the users table in the database.
-"""
-
+User model"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
+
 class User(Base):
+    """ User class """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
