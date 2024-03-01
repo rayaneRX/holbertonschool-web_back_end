30c522a6a91ef22549f238e4ab9bbb34994d6bea
rayaneRX <rayane@rayane>
2024-03-01 14:08:59 +0100

    Hash password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,8 +3,8 @@
 
 import bcrypt
 
-def _hash_password(password):
-    # Generate a salt and hash the password
+
+def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
