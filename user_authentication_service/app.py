4cf512b4792b61dfcc24128bd333fe88c2c97f23
rayaneRX <rayane@rayane>
2024-03-01 15:31:18 +0100

    pycode

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 
-
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+from flask import abort
 from auth import Auth
 
 app = Flask(__name__)
 AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    """ GET route for the root. """
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
@@ -19,6 +25,7 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
