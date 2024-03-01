5e0d0495f7b3b4955daa1f4dfb91f31891cd63fd
rayaneRX <rayane@rayane>
2024-03-01 15:00:38 +0100

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -8,6 +8,7 @@ from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
 
+
 @app.route("/users", methods=["POST"])
 def register_user():
     email = request.form.get("email")
@@ -18,6 +19,7 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
