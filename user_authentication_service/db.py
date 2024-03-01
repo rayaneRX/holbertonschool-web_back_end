6136203bc9b45f41df64d986465e484233bb01c1
rayaneRX <rayane@rayane>
2024-03-01 12:02:38 +0100

    pycode

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ DB module """
+from sqlite3 import IntegrityError
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
