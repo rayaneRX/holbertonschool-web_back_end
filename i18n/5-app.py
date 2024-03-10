8c6402a5df36888318da6c48c8b495edfc8e516b
rayaneRX <rayane@rayane>
2024-03-10 22:38:00 +0100

    test task 5

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
-
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, g, request
 from flask_babel import Babel
 
+
 app = Flask(__name__)
-
-
-class Config:
-    """Config class"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
 babel = Babel(app)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -25,46 +16,53 @@ users = {
 }
 
 
-@babel.localeselector
-def get_locale():
-    """ Get locale from request or user preferences """
-    # Locale from URL parameters
-    locale = request.args.get("locale")
-    if locale and locale in app.config["LANGUAGES"]:
-        return locale
-
-    # Locale from user settings
-    user = getattr(g, 'user', None)
-    if user is not None:
-        user_locale = user.get('locale')
-        if user_locale and user_locale in app.config["LANGUAGES"]:
-            return user_locale
-
-    # Locale from request header
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-# babel.init_app(app, locale_selector=get_locale)
+class Config(object):
+    """ Configuration Babel """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-def get_user():
-    """ Get user from query parameter 'login_as' """
-    user_id = request.args.get("login_as")
-    if user_id:
-        return users.get(int(user_id))
+app.config.from_object(Config)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    """
+    Rendre le modèle avec les informations utilisateur appropriées
+    """
+    return render_template('5-index.html')
+
+
+def get_user() -> dict:
+    """Retrieve user based on login_as parameter"""
+    user_id = request.args.get('login_as')
+    try:
+        user_id = int(user_id)
+        if user_id in users:
+            return users[user_id]
+    except (ValueError, TypeError):
+        pass
     return None
 
 
 @app.before_request
 def before_request():
-    """ Set user before each request if 'login_as' provided """
-    g.user = get_user()
+    """
+    Définir l'utilisateur global s'il est connecté
+    """
+    user = get_user()
+    if user:
+        g.user = user
 
 
-@app.route("/", methods=["GET"])
-def index():
-    """ Index page """
-    return render_template("5-index.html")
+@babel.localeselector
+def get_locale():
+    """
+    Déterminer la langue préférée de l'utilisateur
+    """
+    requested_locale = request.args.get('locale')
+    if requested_locale in Config.LANGUAGES:
+        return requested_locale
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return request.accept_languages.best_match(Config.LANGUAGES)
