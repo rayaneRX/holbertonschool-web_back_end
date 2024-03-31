1f1454213a35c11e51d7abb035a0e40214957917
rayaneRX <rayane@rayane>
2024-03-31 23:09:25 +0200

    unittests_in_js

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,21 +1,30 @@
-// 0-calcul.test.js
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
-  it('should return 4 for inputs 1 and 3', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 for inputs 1 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 for inputs 1.2 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 for inputs 1.5 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+  it('Return the correct sum of 2 positive integers', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(5, 3), 8);
+  }),
+  it('Return the correct sum when 1 argument is a float', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber(3.7, 2), 6);
+  }),
+  it('Return the correct sum when both arguments are floats', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  }),
+  it('Return the correct sum of 2 negative integers', () => {
+    assert.equal(calculateNumber(-1, -3), -4);
+    assert.equal(calculateNumber(-5, -3), -8);
+  }),
+  it('Return the correct sum when 1 argument is positive and the other is negative', () => {
+    assert.equal(calculateNumber(1, -3), -2);
+    assert.equal(calculateNumber(-1, 3), 2);
+  }),
+  it('Throw an error if non-numeric values are provided', () => {
+    assert.throws(() => calculateNumber("a", 2), Error);
+    assert.throws(() => calculateNumber(2, "b"), Error);
+    assert.throws(() => calculateNumber("a", "b"), Error);
+  })
 });
