9bb2d289cefe92cb21ab870af340ac16f73cd56b
rayaneRX <rayane@rayane>
2024-03-31 23:39:49 +0200

    task 8

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -1,13 +1,13 @@
-// api.js
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+app.get("/", (req, res) => {
+  res.send("Welcome to the payment system");
 });
 
-app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+const PORT = 7865;
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,18 +1,19 @@
-// api.test.js
-const request = require('supertest');
-const app = require('./api');
-const { expect } = require('chai');
+const chai = require("chai");
+const request = require("request");
+const expect = chai.expect;
 
-describe('Index page', () => {
-  it('Correct status code', (done) => {
-    request(app)
-      .get('/')
-      .expect(200, done);
+describe("Index page", () => {
+  it("should return correct status code", (done) => {
+    request("http://localhost:7865", (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('Correct result', (done) => {
-    request(app)
-      .get('/')
-      .expect('Welcome to the payment system', done);
+  it("should return correct result", (done) => {
+    request("http://localhost:7865", (error, response, body) => {
+      expect(body).to.equal("Welcome to the payment system");
+      done();
+    });
   });
 });
